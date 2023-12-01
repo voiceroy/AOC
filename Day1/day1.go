560dd1616805b6963b3160a46e299f0c111ac9df
Harishankar G <harishankargaddanakeri@gmail.com>
2023-12-02 00:38:32 +0530

    Day 1 Part 2

--- Day1/day1.go
+++ Day1/day1.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 )
@@ -31,6 +32,26 @@ func part1(data string) int {
 	return calibrationValue
 }
 
+func part2(data string) int {
+	replaceMap := map[*regexp.Regexp]string{
+		regexp.MustCompile("(o)(ne)"):   " 1 ",
+		regexp.MustCompile("(t)(wo)"):   " 2 ",
+		regexp.MustCompile("(thr)(ee)"): " 3 ",
+		regexp.MustCompile("(fo)(ur)"):  " 4 ",
+		regexp.MustCompile("(fi)(ve)"):  " 5 ",
+		regexp.MustCompile("(s)(ix)"):   " 6 ",
+		regexp.MustCompile("(sev)(en)"): " 7 ",
+		regexp.MustCompile("(eig)(ht)"): " 8 ",
+		regexp.MustCompile("(ni)(ne)"):  " 9 ",
+	}
+
+	for k, v := range replaceMap {
+		data = k.ReplaceAllString(data, "$1"+v+"$2")
+	}
+
+	return part1(data)
+}
+
 func main() {
 	file, err := os.ReadFile("input")
 	if err != nil {
@@ -41,4 +62,7 @@ func main() {
 
 	// Part 1
 	fmt.Printf("Part 1: %d\n", part1(fileString))
+
+	// Part 2
+	fmt.Printf("Part 2: %d\n", part2(fileString))
 }
