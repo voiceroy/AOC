d98bfd234567861f45c031256d12a9d0996cf976
Harishankar G <harishankargaddanakeri@gmail.com>
2023-12-02 18:30:25 +0530

    Day 2 Part 2

--- Day2/day2.go
+++ Day2/day2.go
@@ -17,15 +17,15 @@ func validRound(MAXDICES []int, game []int) bool {
 }
 
 func part1(MAXDICES []int, games []string) int {
-	var validGameIdSum int
+	var validGameIDSum int
 
 gameLoop:
-	for i, line := range games {
+	for gameID, game := range games {
 		diceSeenThisRound := []int{0, 0, 0} // [red, green, blue]
 		dieCount := 0
 		dieColor := ""
 
-		rounds := strings.Split(strings.Split(line, ":")[1], ";")
+		rounds := strings.Split(strings.Split(game, ":")[1], ";")
 		for _, round := range rounds {
 			for _, die := range strings.Split(round, ",") {
 				_, _ = fmt.Sscanf(die, "%d %s", &dieCount, &dieColor)
@@ -44,10 +44,48 @@ gameLoop:
 				}
 			}
 		}
-		validGameIdSum += i + 1
+		validGameIDSum += gameID + 1
 	}
 
-	return validGameIdSum
+	return validGameIDSum
+}
+
+func product(game []int) int {
+	result := 1
+	for i := 0; i < len(game); i++ {
+		result *= game[i]
+	}
+
+	return result
+}
+
+func part2(games []string) int {
+	var sumOfProducts int
+
+	for _, game := range games {
+		maxDiceThisRound := []int{0, 0, 0}
+		dieCount := 0
+		dieColor := ""
+
+		rounds := strings.Split(strings.Split(game, ":")[1], ";")
+		for _, round := range rounds {
+			for _, die := range strings.Split(round, ",") {
+				_, _ = fmt.Sscanf(die, "%d %s", &dieCount, &dieColor)
+
+				switch dieColor {
+				case "red":
+					maxDiceThisRound[0] = max(maxDiceThisRound[0], dieCount)
+				case "green":
+					maxDiceThisRound[1] = max(maxDiceThisRound[1], dieCount)
+				case "blue":
+					maxDiceThisRound[2] = max(maxDiceThisRound[2], dieCount)
+				}
+			}
+		}
+		sumOfProducts += product(maxDiceThisRound)
+	}
+
+	return sumOfProducts
 }
 
 func main() {
@@ -62,4 +100,7 @@ func main() {
 
 	// Part 1
 	fmt.Printf("Part 1: %d\n", part1(MAXDICES, fileArray[:len(fileArray)-1]))
+
+	// Part 2
+	fmt.Printf("Part 2: %d\n", part2(fileArray[:len(fileArray)-1]))
 }
