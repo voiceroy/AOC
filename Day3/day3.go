2fcd4ec1f0b71988cddc2405692ba13e2d474017
Harishankar G <harishankargaddanakeri@gmail.com>
2023-12-05 00:02:33 +0530

    Day 3 Part 2 (An abomination of code)

--- Day3/day3.go
+++ Day3/day3.go
@@ -15,9 +15,13 @@ type number struct {
 	columnEnd   int
 }
 
-const validSymbols = "+-*/@&$#=%"
+type gear struct {
+	row      int
+	column   int
+	adjacent int
+}
 
-func checkNumber(data []string, possibleNumber number) bool {
+func checkNumber(data []string, possibleNumber number, validSymbols string) bool {
 	var validNumber bool
 	columnStart := max(0, possibleNumber.columnStart-1)
 	columnEnd := min(possibleNumber.columnEnd+1, len(data[0]))
@@ -38,8 +42,7 @@ func checkNumber(data []string, possibleNumber number) bool {
 	return validNumber
 }
 
-func part1(data []string) int {
-	var sumPartNumbers int
+func getNumberLocations(data []string) []number {
 	var numberLocations []number
 
 	for row, line := range data {
@@ -70,14 +73,69 @@ func part1(data []string) int {
 		}
 	}
 
+	return numberLocations
+}
+
+func part1(data []string) int {
+	var sumPartNumbers int
+	var numberLocations = getNumberLocations(data)
+
 	for _, num := range numberLocations {
-		if checkNumber(data, num) {
+		if checkNumber(data, num, "+-*/@&$#=%") {
 			value, _ := strconv.Atoi(num.value)
 			sumPartNumbers += value
 		}
 	}
 
 	return sumPartNumbers
+}
+
+func getGearLocations(data []string) []gear {
+	var gearLocations []gear
+	for i, line := range data {
+		for j, char := range line {
+			if string(char) == "*" {
+				gearLocations = append(gearLocations, gear{i, j, 0})
+			}
+		}
+	}
+
+	return gearLocations
+}
+
+func part2(data []string) int {
+	var sumOfGearRatios int
+	var numberLocations []number
+	var gearLocations = getGearLocations(data)
+
+	for _, num := range getNumberLocations(data) {
+		if checkNumber(data, num, "*") {
+			numberLocations = append(numberLocations, num)
+		}
+	}
+
+	for _, currentGear := range gearLocations {
+		nums := make([]number, 0)
+		for _, num := range numberLocations {
+			if (num.columnStart <= currentGear.column && currentGear.column+1 <= num.columnEnd && max(0, num.row-1) <= currentGear.row && currentGear.row <= min(num.row+1, len(data))) || ((currentGear.column == num.columnStart-1 || currentGear.column == num.columnEnd) && (max(0, num.row-1) <= currentGear.row && currentGear.row <= min(num.row+1, len(data)))) {
+				currentGear.adjacent++
+				nums = append(nums, num)
+			}
+		}
+
+		if len(nums) == 2 {
+			gearRatio := 1
+
+			for _, num := range nums {
+				value, _ := strconv.Atoi(num.value)
+				gearRatio *= value
+			}
+			fmt.Printf("Gear Pairs: %v\n", nums)
+			sumOfGearRatios += gearRatio
+		}
+	}
+
+	return sumOfGearRatios
 }
 
 func main() {
@@ -91,4 +149,7 @@ func main() {
 
 	// Part 1
 	fmt.Printf("Part 1: %d\n", part1(fileArray))
+
+	// Part 2
+	fmt.Printf("Part 2: %d\n", part2(fileArray))
 }
