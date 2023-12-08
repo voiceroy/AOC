211028b290621bb349db50270f28a91e34f01d01
Harishankar G <harishankargaddanakeri@gmail.com>
2023-12-08 19:07:06 +0530

    Day 8 Part 2

--- Day8/day8.go
+++ Day8/day8.go
@@ -3,17 +3,16 @@ package main
 import (
 	"fmt"
 	"os"
-	"regexp"
 	"strings"
 )
 
 func makeMap(data []string) map[string][]string {
 	nodeMap := make(map[string][]string)
-	pattern := regexp.MustCompile(`(\w+) = \((\w+), (\w+)\)`)
 
+	currentNode, leftNode, rightNode := "", "", ""
 	for _, mapping := range data {
-		matches := pattern.FindAllStringSubmatch(mapping, -1)
-		nodeMap[matches[0][1]] = []string{matches[0][2], matches[0][3]}
+		fmt.Sscanf(mapping, "%s = (%3s, %3s)", &currentNode, &leftNode, &rightNode)
+		nodeMap[currentNode] = []string{leftNode, rightNode}
 	}
 
 	return nodeMap
@@ -35,6 +34,52 @@ func partOne(directions string, nodeMap map[string][]string) int {
 	}
 }
 
+func gcd(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+
+	return a
+}
+
+func lcm(integers ...int) int {
+	result := integers[0] * integers[1] / gcd(integers[0], integers[1])
+
+	for i := 2; i < len(integers); i++ {
+		result = lcm(result, integers[i])
+	}
+
+	return result
+}
+
+func partTwo(directions string, nodeMap map[string][]string) int {
+	var stepsForEachNode []int
+
+	for node := range nodeMap {
+		if strings.HasSuffix(node, "A") {
+			currentNode := node
+
+			for i := 0; ; i++ {
+				if strings.HasSuffix(currentNode, "Z") {
+					stepsForEachNode = append(stepsForEachNode, i)
+					break
+				}
+
+				direction := directions[i%len(directions)]
+				if direction == 76 {
+					currentNode = nodeMap[currentNode][0]
+				} else {
+					currentNode = nodeMap[currentNode][1]
+				}
+			}
+		}
+	}
+
+	return lcm(stepsForEachNode...)
+}
+
 func main() {
 	file, err := os.ReadFile("input")
 	if err != nil {
@@ -48,4 +93,7 @@ func main() {
 
 	// Part 1
 	fmt.Printf("Part 1: %d\n", partOne(directions, nodeMap))
+
+	// Part 2
+	fmt.Printf("Part 2: %d\n", partTwo(directions, nodeMap))
 }
