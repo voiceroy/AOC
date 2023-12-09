7b5293968628da30645c64e2a6908c627578ce67
Harishankar G <harishankargaddanakeri@gmail.com>
2023-12-09 23:24:35 +0530

    Update day9.go

--- Day9/day9.go
+++ Day9/day9.go
@@ -9,9 +9,9 @@ import (
 
 func parseInts(data []string) [][]int {
 	var parsedInts [][]int
-
 	for i, row := range data {
 		parsedInts = append(parsedInts, []int{})
+
 		for _, num := range strings.Fields(row) {
 			value, _ := strconv.Atoi(num)
 			parsedInts[i] = append(parsedInts[i], value)
