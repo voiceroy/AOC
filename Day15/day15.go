fee986ca05ca73255711196923f9bf07cf52721e
Harishankar G <harishankargaddanakeri@gmail.com>
2023-12-15 12:51:59 +0530

    Day 15 Part 2

--- Day15/day15.go
+++ Day15/day15.go
@@ -3,8 +3,14 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
+
+type lens struct {
+	label       string
+	focalLength int
+}
 
 func hash(str string) int {
 	var currentHash int
@@ -26,6 +32,51 @@ func partOne(data string) int {
 	return hashSum
 }
 
+func findIndex(array []lens, label string) (int, bool) {
+	for i := range array {
+		if array[i].label == label {
+			return i, true
+		}
+	}
+
+	return -1, false
+}
+
+func partTwo(data string) int {
+	var lensPower int
+	var boxes [256][]lens
+
+	for _, seq := range strings.Split(data, ",") {
+		if strings.Contains(seq, "=") {
+			power, _ := strconv.Atoi(string(seq[len(seq)-1]))
+			label := seq[:len(seq)-2]
+			location := hash(label)
+
+			currentLens := lens{label, power}
+			if index, found := findIndex(boxes[location], currentLens.label); found {
+				boxes[location][index] = currentLens
+			} else {
+				boxes[location] = append(boxes[location], lens{label, power})
+			}
+		} else {
+			label := seq[:len(seq)-1]
+			location := hash(label)
+
+			if index, found := findIndex(boxes[location], label); found {
+				boxes[location] = append(boxes[location][:index], boxes[location][index+1:]...)
+			}
+		}
+	}
+
+	for i, box := range boxes {
+		for j, currentLens := range box {
+			lensPower += (i + 1) * (j + 1) * currentLens.focalLength
+		}
+	}
+
+	return lensPower
+}
+
 func main() {
 	file, err := os.ReadFile("input")
 	if err != nil {
@@ -37,4 +88,7 @@ func main() {
 
 	// Part 1
 	fmt.Printf("Part 1: %d\n", partOne(fileString))
+
+	// Part 2
+	fmt.Printf("Part 2: %d\n", partTwo(fileString))
 }
